7bf5d04e8accfa64cea081e48c1f5b74dc9626de
Arthlight <arthurpbh@gmail.com>
2020-06-02 18:59:45 +0200

    refactored Encoding into their own function

--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
 import (
-	api "Spotify-Visualizer/api/v1"
+	"Spotify-Visualizer/spotify"
 	"fmt"
-	"github.com/go-chi/chi"
-	"net/http"
 )
-
-const port = ":4000"
+//const port = ":4000"
 
 func main() {
-	router := chi.NewRouter()
+/*	router := chi.NewRouter()
 	v1Handler := api.NewApiRouter()
 	router.Mount("/v1", v1Handler)
 
 	fmt.Printf("Started http server on %s\n", port)
 	err := http.ListenAndServe(port, router)
-	fmt.Println(err)
-
+	fmt.Println(err)*/
+	fmt.Println(spotify.GetAccessToken())
 }
--- spotify/spotify.go
+++ spotify/spotify.go
@@ -6,12 +6,14 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/joho/godotenv/autoload"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strings"
 )
 
 func GetAccessToken(refreshToken string) (string, error) {
+	client := http.Client{}
 	body, err := json.Marshal(map[string]string{
 		"grant_type": "refresh_token",
 		"refresh_token": refreshToken,
@@ -21,26 +23,43 @@ func GetAccessToken(refreshToken string) (string, error) {
 		return "", err
 	}
 
-	AuthHeaderString := os.Getenv("CLIENT_ID") + ":" + os.Getenv("CLIENT_SECRET")
-
-	var sb strings.Builder
-	encoder := base64.NewEncoder(base64.StdEncoding, &sb)
-	_, err = encoder.Write([]byte(AuthHeaderString))
+	encodedAuthHeader, err := getEncodedAuthHeader(os.Getenv("CLIENT_ID") + ":" + os.Getenv("CLIENT_SECRET"))
 	if err != nil {
-		fmt.Println("Error encoding AuthHeaderString into base64: ", err)
-		return "", nil
+		fmt.Println("Error while trying to encode auth header: ", err)
 	}
-	err = encoder.Close()
-	if err != nil {
-		fmt.Println("Error closing base64 encoder: ", err)
-		return "", nil
-	}
-	encodedAuthHeaderString := sb.String()
-
 	request, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBuffer(body))
 	if err != nil {
 		fmt.Printf("Error while trying to create a new request: %v", err)
 	}
 	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Set("Authorization", "Basic " + encodedAuthHeaderString)
+	request.Header.Set("Authorization", "Basic " + encodedAuthHeader)
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Error while trying to send request: ", err)
+	}
+	defer response.Body.Close()
+
+	body, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error while trying to read response body: ", err)
+	}
+
+	return fmt.Sprint(string(body)), nil
+}
+
+func getEncodedAuthHeader(credentials string) (string, error) {
+	var sb strings.Builder
+	encoder := base64.NewEncoder(base64.StdEncoding, &sb)
+	_, err := encoder.Write([]byte(credentials))
+	if err != nil {
+		return "", err
+	}
+	err = encoder.Close()
+	if err != nil {
+		return "", err
+	}
+	encodedAuthHeader := sb.String()
+
+	return encodedAuthHeader, nil
 }
